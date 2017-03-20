5bd9f0f15b3e77ec9754f0c2004b2daf323ac889
Helena McCullough <Helena@Helenas-MacBook-Pro.local>
2017-03-20 15:37:57 +0100

    #babysteps - Created Magic_Machine

---
+++ KonzTestApp/Magic_Machine.swift
@@ -0,0 +1,18 @@
+//
+//  Magic_Machine.swift
+//  KonzTestApp
+//
+//  Created by Helena McCullough on 20.03.17.
+//  Copyright © 2017 Helena McCullough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Magic_Machine {
+    
+    var raw_image : UIImage
+    
+    var solution_ID : Int
+    
+}
